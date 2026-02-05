37532a9fbadb4426d3d96f75fdc6b566b30b3be1
fdzul <felipe.dzul.m@gmail.com>
2026-02-04 21:24:29 -0600

    This commit update the pkg

--- R/data.R
+++ R/data.R
@@ -7,7 +7,7 @@
 #'   \item{CVEGEO}{the ids.}
 #'   \item{CVE_ENT}{is the numeric key of state.}
 #'   \item{CVE_MUN}{is the numeric key to the Municipality}
-#'   \item{NOMGEO}{is the numeric key to the Sectors}
+#'   \item{NOMGEO}{is the name of the Municipality}
 #'   \item{ACC_2003}{accident cases from 2003}
 #'   \item{ACC_2004}{accident cases from 2004}
 #'   \item{ACC_2005}{accident cases from 2005}
@@ -37,3 +37,123 @@
 #' @source \url{https://pautas.ine.mx/transparencia/mapas/}
 #' @details object sf of a municipal dataset of accident cases in Mexico from 2003 to 2025.
 "accidents"
+
+#' Cardiovascular diseases dataset
+#'
+#' Municipal dataset containing cardiovascular cases in Mexico from 2003 to 2025
+#'
+#' @format A sf object with 2465 rows and 29 variables:
+#' \describe{
+#'   \item{CVEGEO}{the ids.}
+#'   \item{CVE_ENT}{is the numeric key of state.}
+#'   \item{CVE_MUN}{is the numeric key to the Municipality}
+#'   \item{NOMGEO}{is the name of the Municipality}
+#'   \item{CD_2003}{cardiovascular cases from 2003}
+#'   \item{CD_2004}{cardiovascular cases from 2004}
+#'   \item{CD_2005}{cardiovascular cases from 2005}
+#'   \item{CD_2006}{cardiovascular cases from 2006}
+#'   \item{CD_2007}{cardiovascular cases from 2007}
+#'   \item{CD_2008}{cardiovascular cases from 2008}
+#'   \item{CD_2009}{cardiovascular cases from 2009}
+#'   \item{CD_2010}{cardiovascular cases from 2010}
+#'   \item{CD_2011}{cardiovascular cases from 2011}
+#'   \item{CD_2012}{cardiovascular cases from 2012}
+#'   \item{CD_2013}{cardiovascular cases from 2013}
+#'   \item{CD_2014}{cardiovascular cases from 2014}
+#'   \item{CD_2015}{cardiovascular cases from 2015}
+#'   \item{CD_2016}{cardiovascular cases from 2016}
+#'   \item{CD_2017}{cardiovascular cases from 2017}
+#'   \item{CD_2018}{cardiovascular cases from 2018}
+#'   \item{CD_2019}{cardiovascular cases from 2019}
+#'   \item{CD_2020}{cardiovascular cases from 2020}
+#'   \item{CD_2021}{cardiovascular cases from 2021}
+#'   \item{CD_2022}{cardiovascular cases from 2022}
+#'   \item{CD_2023}{cardiovascular cases from 2023}
+#'   \item{CD_2024}{cardiovascular cases from 2024}
+#'   \item{CD_2025}{cardiovascular cases from 2025}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://pautas.ine.mx/transparencia/mapas/}
+#' @details object sf of a municipal dataset of cardiovascular cases in Mexico from 2003 to 2025.
+"cardiovascular_diseases"
+
+#' Cardiovascular diseases dataset
+#'
+#' Municipal dataset containing cardiovascular cases in Mexico from 2003 to 2025
+#'
+#' @format A sf object with 2465 rows and 29 variables:
+#' \describe{
+#'   \item{CVEGEO}{the ids.}
+#'   \item{CVE_ENT}{is the numeric key of state.}
+#'   \item{CVE_MUN}{is the numeric key to the Municipality}
+#'   \item{NOMGEO}{is the name of the Municipality}
+#'   \item{DIA_2003}{diabetes cases from 2003}
+#'   \item{DIA_2004}{diabetes cases from 2004}
+#'   \item{DIA_2005}{diabetes cases from 2005}
+#'   \item{DIA_2006}{diabetes cases from 2006}
+#'   \item{DIA_2007}{diabetes cases from 2007}
+#'   \item{DIA_2008}{diabetes cases from 2008}
+#'   \item{DIA_2009}{diabetes cases from 2009}
+#'   \item{DIA_2010}{diabetes cases from 2010}
+#'   \item{DIA_2011}{diabetes cases from 2011}
+#'   \item{DIA_2012}{diabetes cases from 2012}
+#'   \item{DIA_2013}{diabetes cases from 2013}
+#'   \item{DIA_2014}{diabetes cases from 2014}
+#'   \item{DIA_2015}{diabetes cases from 2015}
+#'   \item{DIA_2016}{diabetes cases from 2016}
+#'   \item{DIA_2017}{diabetes cases from 2017}
+#'   \item{DIA_2018}{diabetescases from 2018}
+#'   \item{DIA_2019}{diabetes cases from 2019}
+#'   \item{DIA_2020}{diabetes cases from 2020}
+#'   \item{DIA_2021}{diabetes cases from 2021}
+#'   \item{DIA_2022}{diabetes cases from 2022}
+#'   \item{DIA_2023}{diabetes cases from 2023}
+#'   \item{DIA_2024}{diabetes cases from 2024}
+#'   \item{DIA_2025}{diabetes cases from 2025}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://pautas.ine.mx/transparencia/mapas/}
+#' @details object sf of a municipal dataset of diabetes cases in Mexico from 2003 to 2025.
+"diabetes"
+
+#' Cardiovascular diseases dataset
+#'
+#' Municipal dataset containing cardiovascular cases in Mexico from 2003 to 2025
+#'
+#' @format A sf object with 2465 rows and 15 variables:
+#' \describe{
+#'   \item{CVEGEO}{the ids.}
+#'   \item{CVE_ENT}{is the numeric key of state.}
+#'   \item{CVE_MUN}{is the numeric key to the Municipality}
+#'   \item{NOMGEO}{is the name of the Municipality}
+#'   \item{EPOC_2003}{epoc cases from 2003}
+#'   \item{EPOC_2004}{epoc cases from 2004}
+#'   \item{EPOC_2005}{epoc cases from 2005}
+#'   \item{EPOC_2006}{epoc cases from 2006}
+#'   \item{EPOC_2007}{epoc cases from 2007}
+#'   \item{EPOC_2008}{epoc cases from 2008}
+#'   \item{EPOC_2009}{epoc cases from 2009}
+#'   \item{EPOC_2010}{epoc cases from 2010}
+#'   \item{EPOC_2011}{epoc cases from 2011}
+#'   \item{EPOC_2012}{epoc cases from 2012}
+#'   \item{EPOC_2013}{epoc cases from 2013}
+#'   \item{EPOC_2014}{epoc cases from 2014}
+#'   \item{EPOC_2015}{epoc cases from 2015}
+#'   \item{EPOC_2016}{epoc cases from 2016}
+#'   \item{EPOC_2017}{epoc cases from 2017}
+#'   \item{EPOC_2018}{epoc cases from 2018}
+#'   \item{EPOC_2019}{epoc cases from 2019}
+#'   \item{EPOC_2020}{epoc cases from 2020}
+#'   \item{EPOC_2021}{epoc cases from 2021}
+#'   \item{EPOC_2022}{epoc cases from 2022}
+#'   \item{EPOC_2023}{epoc cases from 2023}
+#'   \item{EPOC_2024}{epoc cases from 2024}
+#'   \item{EPOC_2025}{epoc cases from 2025}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://pautas.ine.mx/transparencia/mapas/}
+#' @details object sf of a municipal dataset of epoc cases in Mexico from 2003 to 2025.
+"epoc"
